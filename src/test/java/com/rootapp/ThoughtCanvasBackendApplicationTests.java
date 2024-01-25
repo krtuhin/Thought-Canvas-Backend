7546460d80e3d073c3231f1c7fe78d393d5bb84d
krtuhin <tuhin.km2774@gmail.com>
2024-01-25 12:11:08 +0530

    Project Created

---
+++ src/test/java/com/rootapp/ThoughtCanvasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtCanvasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
