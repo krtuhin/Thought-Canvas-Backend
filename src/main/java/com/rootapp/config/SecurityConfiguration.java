561c318e46660899d8d12be1e24d0626acdddc21
krtuhin <tuhin.km2774@gmail.com>
2024-02-23 21:50:49 +0530

    Basic Implementation of Swagger-UI

--- src/main/java/com/rootapp/config/SecurityConfiguration.java
+++ src/main/java/com/rootapp/config/SecurityConfiguration.java
@@ -15,12 +15,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.rootapp.security.JwtAuthenticationEntryPoint;
 import com.rootapp.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @EnableMethodSecurity(prePostEnabled = true) // add role based security
 public class SecurityConfiguration {
 
@@ -37,7 +39,8 @@ public class SecurityConfiguration {
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     // list of public URLs
-    private static final String[] PUBLIC_URLS = { "/api/auth/**" };
+    private static final String[] PUBLIC_URLS = { "/api/auth/**", "/v3/api-docs", "/v2/api-docs",
+            "/swagger-resources/**", "/swagger-ui/**", "/webjars/**" };
 
     // spring security configuration
     @Bean
